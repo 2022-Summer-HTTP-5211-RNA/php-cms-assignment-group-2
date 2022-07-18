c7fbe2be957e2287a266fde167fafe264a6e8407
Lebron Brian Ssekalegga <lebronbrian23@gmail.com>
2022-07-18 16:46:18 -0400

    Update content_block_edit.php

--- admin/content_block_edit.php
+++ admin/content_block_edit.php
@@ -6,8 +6,6 @@ include( 'includes/functions.php' );
 
 secure();
 
-include( 'includes/header.php' );
-
 if( !isset( $_GET['id'] ) )
 {
 
@@ -16,22 +14,40 @@ if( !isset( $_GET['id'] ) )
 
 }
 
-if( isset( $_POST['name'] ) )
+if( isset( $_FILES['photo'] ) )
 {
 
-  if( $_POST['name'] and $_POST['description'] )
+  if( isset( $_FILES['photo'] ) )
   {
 
-    $query = 'UPDATE about SET
-      name = "'.mysqli_real_escape_string( $connect, $_POST['name'] ).'",
-      description = "'.mysqli_real_escape_string( $connect, $_POST['description'] ).'"
-      WHERE id = '.$_GET['id'].'
-      LIMIT 1';
-    mysqli_query( $connect, $query );
+    if( $_FILES['photo']['error'] == 0 )
+    {
 
-    set_message( 'About information has been updated' );
+      switch( $_FILES['photo']['type'] )
+      {
+        case 'image/png':
+          $type = 'png';
+          break;
+        case 'image/jpg':
+        case 'image/jpeg':
+          $type = 'jpeg';
+          break;
+        case 'image/gif':
+          $type = 'gif';
+          break;
+      }
+
+      $query = 'UPDATE content_block SET
+        photo = "data:image/'.$type.';base64,'.base64_encode( file_get_contents( $_FILES['photo']['tmp_name'] ) ).'"
+        WHERE id = '.$_GET['id'].'
+        LIMIT 1';
+      mysqli_query( $connect, $query );
+
+    }
 
   }
+
+  set_message( 'Content block photo has been updated' );
 
   header( 'Location: content_blocks.php' );
   die();
@@ -42,8 +58,24 @@ if( isset( $_POST['name'] ) )
 if( isset( $_GET['id'] ) )
 {
 
+  if( isset( $_GET['delete'] ) )
+  {
+
+    $query = 'UPDATE content_block SET
+      photo = ""
+      WHERE id = '.$_GET['id'].'
+      LIMIT 1';
+    $result = mysqli_query( $connect, $query );
+
+    set_message( 'Content block photo has been deleted' );
+
+    header( 'Location: content_blocks.php' );
+    die();
+
+  }
+
   $query = 'SELECT *
-    FROM about
+    FROM content_block
     WHERE id = '.$_GET['id'].'
     LIMIT 1';
   $result = mysqli_query( $connect, $query );
@@ -59,42 +91,44 @@ if( isset( $_GET['id'] ) )
   $record = mysqli_fetch_assoc( $result );
 
 }
+include( 'includes/header.php' );
+
+include 'includes/wideimage/WideImage.php';
 
 ?>
 
-<h2>Edit Content block</h2>
+<h2>Edit Content block Image</h2>
 
-<form method="post">
+<p>
+  Note: For best results, photos should be approximately 800 x 800 pixels.
+</p>
 
-  <label for="name">Name:</label>
-  <input type="text" name="name" id="name" value="<?php echo htmlentities( $record['name'] ); ?>">
+<?php if( $record['photo'] ): ?>
+
+  <?php
+
+  $data = base64_decode( explode( ',', $record['photo'] )[1] );
+  $img = WideImage::loadFromString( $data );
+  $data = $img->resize( 200, 200, 'outside' )->crop( 'center', 'center', 200, 200 )->asString( 'jpg', 70 );
+
+  ?>
+  <p><img src="data:image/jpg;base64,<?php echo base64_encode( $data ); ?>" width="200" height="200"></p>
+  <p><a href="content_block_photo.php?id=<?php echo $_GET['id']; ?>&delete"><i class="fas fa-trash-alt"></i> Delete this Photo</a></p>
+
+<?php endif; ?>
+
+<form method="post" enctype="multipart/form-data">
+
+  <label for="photo">Photo:</label>
+  <input type="file" name="photo" id="photo">
 
   <br>
-  <label for="type">Type:</label>
-  <input type="text" name="type" id="type" value="<?php echo htmlentities( $record['type'] ); ?>">
-  <br>
 
-  <label for="description">Description:</label>
-  <textarea type="text" name="description" id="description" rows="5"><?php echo htmlentities( $record['description'] ); ?></textarea>
-
-  <script>
-
-  ClassicEditor
-    .create( document.querySelector( '#description' ) )
-    .then( editor => {
-        console.log( editor );
-    } )
-    .catch( error => {
-        console.error( error );
-    } );
-
-  </script>
-  <br>
-  <input type="submit" value="Edit Content block">
+  <input type="submit" value="Save Photo">
 
 </form>
 
-<p><a href="projects.php"><i class="fas fa-arrow-circle-left"></i> Return to Content block</a></p>
+<p><a href="content_blocks.php"><i class="fas fa-arrow-circle-left"></i> Return to Content block</a></p>
 
 
 <?php
