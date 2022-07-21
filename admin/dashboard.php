72356b5a056b5da11d4d562adc08546496ccc31d
LebronBrianSinza <lebronbrian23@gmail.com>
2022-07-21 12:41:50 -0400

    added icons on dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -13,32 +13,33 @@ include( 'includes/header.php' );
 <ul id="dashboard">
   <li>
     <a href="projects.php">
-      Manage Projects
+        <i class="fa-solid fa-list-check"></i> Manage Projects
     </a>
   </li>
   <li>
     <a href="skills.php">
-      Manage Skills
+        <i class="fa-solid fa-screwdriver-wrench"></i> Manage Skills
     </a>
   </li>
   <li>
     <a href="social_media.php">
-      Manage SocialMedia
+        <i class="fa-solid fa-hashtag"></i> Manage Social Media
     </a>
   </li>
   <li>
     <a href="users.php">
-      Manage Users
+        <i class="fa-solid fa-users"></i>
+        Manage Users
     </a>
   </li>
   <li>
     <a href="content_blocks.php">
-      Manage Content blocks
+        <i class="fa-solid fa-folder-open"></i> Manage Content blocks
     </a>
   </li>
   <li>
     <a href="logout.php">
-      Logout
+        <i class="fa-solid fa-arrow-right-from-bracket"></i> Logout
     </a>
   </li>
 </ul>
